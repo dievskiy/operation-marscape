7a750d23b1a039604c13f58c275f4d4265176d79
dievskiy <potekhin.ruslan@gmail.com>
2020-11-19 14:55:43 +0200

    end trigger added

---
+++ Assets/Scripts/LevelContol.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static SceneController;
+
+
+public class LevelContol : MonoBehaviour
+{
+    private const string PLAYER_TAG = "Player";
+
+    private GameObject player;
+    void Start()
+    {
+        player = GameObject.FindWithTag(PLAYER_TAG);
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == PLAYER_TAG) {
+            SceneController.LoadScene(SceneType.LEVEL_1);
+        }
+    }
+
+}
