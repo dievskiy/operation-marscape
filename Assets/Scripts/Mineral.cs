e748bd21fbfc650db2f4c322ae73323ab00509e2
alekskos <kososenaleksi@gmai.com>
2020-11-20 17:21:29 +0200

    Mineral pickup and progress bar

---
+++ Assets/Scripts/Mineral.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mineral : MonoBehaviour
+{
+    private float Value=25.0f;
+    private float Inventory=0f;
+    private float MaxValue = 100f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PickUpMineral()
+    {
+        if (Inventory < MaxValue)
+        {
+            Inventory += Value;
+        }
+    }
+
+    public float GetInventory()
+    {
+        return Inventory;
+    }
+
+    public void SaveScore()
+    {
+        PlayerPrefs.SetFloat("Inventory", Inventory);
+    }
+
+    public void FetchInventory()
+    {
+        Inventory = PlayerPrefs.GetFloat("Inventory");
+    }
+}
---
+++ Assets/Scripts/ProgressBar.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ProgressBar : MonoBehaviour
+{
+    private Slider slider;
+
+    private float FillSpeed = 0.2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        slider = gameObject.GetComponent<Slider>();
+        slider.value = 0.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Progress()
+    {
+        slider.value += 0.25f;
+        slider.value += FillSpeed * Time.deltaTime;
+        Debug.Log(slider.value);
+    }
+}
