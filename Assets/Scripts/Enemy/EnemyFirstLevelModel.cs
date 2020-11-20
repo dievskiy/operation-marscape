c9cfba85d754ffc10930bdc56ba993200f76a6cf
dievskiy <potekhin.ruslan@gmail.com>
2020-11-20 13:26:39 +0200

    base model for enemy

---
+++ Assets/Scripts/Enemy/EnemyFirstLevelModel.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFirstLevelModel : EnemyModel
+{
+     public EnemyFirstLevelModel()
+     {
+        hp = 100;
+     }
+
+    override public void AttackPlayer()
+    {
+        // todo
+    }
+
+    override public void Die()
+    {
+        // todo
+    }
+}
---
+++ Assets/Scripts/Enemy/EnemyMovementController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovementController : MonoBehaviour
+{
+
+    public Vector3[] navigationPositions;
+    public float speed = 0.07f;
+    private int currentTarget; 
+
+
+    void FixedUpdate()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, navigationPositions[currentTarget], speed);
+
+        // when target position is reached, switch to another one
+        if (transform.position == navigationPositions[currentTarget])
+        {
+            if (currentTarget < navigationPositions.Length - 1) currentTarget ++;
+            else currentTarget = 0;
+        }
+    }
+}
