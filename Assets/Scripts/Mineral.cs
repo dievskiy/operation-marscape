d0efc30ea9521e797083c4e61fefc256c0fc5b76
zantze <tuomiotulee@gmail.com>
2020-11-23 18:53:17 +0200

    Created visual for mineral

--- Assets/Scripts/Mineral.cs
+++ Assets/Scripts/Mineral.cs
@@ -8,6 +8,9 @@ public class Mineral : MonoBehaviour
     private float Inventory=0f;
     private float MaxValue = 100f;
 
+    public GameObject visual;
+    private float timer;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,7 +20,19 @@ public class Mineral : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 visualPosition = transform.position;
+
+        // Move the visual element in a sine wave
+        visualPosition.y += Mathf.Sin(timer) / 4;
+
+        // set the modified position to the visual
+        visual.transform.position = visualPosition;
+
+        // rotate the visual for nice effect
+        visual.transform.Rotate(visual.transform.up * 60 * Time.deltaTime);
+
+        // sine wave requires a time variable, use this
+        timer += 2.2f * Time.deltaTime;
     }
 
     public void PickUpMineral()
