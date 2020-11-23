0a44dc08a40bf4082199a51b67d2a7ab2f4eb4f2
zantze <tuomiotulee@gmail.com>
2020-11-23 17:51:06 +0200

    comments

--- Assets/Bullet.cs
+++ Assets/Bullet.cs
@@ -16,12 +16,14 @@ public class Bullet : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // if the distance between camera and the bullet grows too big, remove the bullet
         float distance = Vector2.Distance(transform.position, Camera.main.transform.position);
         if (distance > maxDistance)
         {
             Destroy(gameObject);
         }
 
+        // move projectile to right
         transform.Translate(transform.right * projectileSpeed * Time.deltaTime);
 
     }
