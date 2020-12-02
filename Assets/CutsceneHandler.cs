c3313fba24d77e7fd679b3acad7623ded8897cc2
zantze <tuomiotulee@gmail.com>
2020-12-02 12:43:22 +0200

    play intro cutscene on new game

---
+++ Assets/CutsceneHandler.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class CutsceneHandler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private VideoPlayer player;
+
+    void Start()
+    {
+        player = GetComponent<VideoPlayer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ( (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Escape)) || (player.time >= player.length))
+        {
+            SceneController.LoadScene(SceneController.SceneType.LEVEL_1);
+        }
+    }
+}
--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -7,12 +7,15 @@ public class SceneController : MonoBehaviour
 {
     public enum SceneType
     {
+        MAIN_MENU,
+        CUTSCENE,
         LEVEL_1
     }
 
     public static void LoadScene(SceneType type){
         switch(type){
-            case SceneType.LEVEL_1: SceneManager.LoadScene(0, LoadSceneMode.Single); break;
+            case SceneType.CUTSCENE: SceneManager.LoadScene("IntroCutscene", LoadSceneMode.Single); break;
+            case SceneType.LEVEL_1: SceneManager.LoadScene("TestScene_1", LoadSceneMode.Single); break;
             default: break;
         }
         
