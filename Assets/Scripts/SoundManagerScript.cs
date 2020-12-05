ab9d5d6797d089c54a166835fbd20f7164559a5f
alekskos <kososenaleksi@gmai.com>
2020-12-05 11:29:58 +0200

    Soundmanager and endscreen bug fixes

--- Assets/Scripts/SoundManagerScript.cs
+++ Assets/Scripts/SoundManagerScript.cs
@@ -12,14 +12,13 @@ public class SoundManagerScript : MonoBehaviour
         alienStep1, alienStep2, alienStep3, alienDeath;
 
     //Variable for AudioSource
-    static AudioSource audioSrc;
+    static AudioSource gameAudio;
 
     //Bool variable for pausing the music of the game
     public static bool musicPaused=false;
 
     void Start()
     {
-
         //Setting the AudioClip variables to right soundfiles from Resources
         mainMenuTheme = Resources.Load<AudioClip>("Mainmenu_theme");
         levelOneTheme = Resources.Load<AudioClip>("theme_01");
@@ -43,7 +42,7 @@ public class SoundManagerScript : MonoBehaviour
         alienDeath = Resources.Load<AudioClip>("alien_growl");
 
         //Setting the AudioSource variable to AudioSource Component
-        audioSrc = GetComponent<AudioSource>();
+        gameAudio = GetComponent<AudioSource>();
 
     }
 
@@ -52,93 +51,90 @@ public class SoundManagerScript : MonoBehaviour
         //Using SceneManager to configure which scene is active
         string scene = SceneManager.GetActiveScene().name;
 
-
         //Setting the right music for each level
-        if (scene == "MainMenu" && !audioSrc.isPlaying)
+        if (scene == "MainMenu" && !gameAudio.isPlaying)
         {
             PlaySound("mainMenu");
         }
 
-        if ((scene == "Level1" || scene == "Level_2") && !audioSrc.isPlaying && !musicPaused)
+        if ((scene == "Level1" || scene == "Level_2") && !gameAudio.isPlaying && !musicPaused)
         {
             PlaySound("theme01");
-        }
-
-        //Pauses AudioSource if musicPaused is set to true
-        if (musicPaused)
-        {
-            audioSrc.Stop();
         }
     }
 
     //Public method for Playing different sounds of the game
     //the string clip is given to switch-clause and volumes are condigured here
-    public static void PlaySound (string clip)
+    public static void PlaySound (string sound)
     {
-        switch (clip)
+        if (!musicPaused)
         {
-            case "lazerGun": 
-                audioSrc.PlayOneShot(lazerGunSound, 0.6f);
-                break;
-            case "alienLazer":
-                audioSrc.PlayOneShot(alienGunSound, 0.6f);
-                break;
-            case "lazerHit":
-                audioSrc.PlayOneShot(lazerHitSound);
-                break;
+            switch (sound)
+            {
+                case "lazerGun":
+                    gameAudio.PlayOneShot(lazerGunSound, 0.6f);
+                    break;
+                case "alienLazer":
+                    gameAudio.PlayOneShot(alienGunSound, 0.6f);
+                    break;
+                case "lazerHit":
+                    gameAudio.PlayOneShot(lazerHitSound);
+                    break;
 
-            case "mineralCollect":
-                audioSrc.PlayOneShot(mineralPickupSound, 0.7f);
-                break;
-            case "jump":
-                audioSrc.PlayOneShot(jumpSound);
-                break;
+                case "mineralCollect":
+                    gameAudio.PlayOneShot(mineralPickupSound, 0.7f);
+                    break;
+                case "jump":
+                    gameAudio.PlayOneShot(jumpSound);
+                    break;
 
-            case "maxStep1":
-                audioSrc.PlayOneShot(maxStep1, 0.4f);
-                break;
-            case "maxStep2":
-                audioSrc.PlayOneShot(maxStep2, 0.35f);
-                break;
-            case "maxStep3":
-                audioSrc.PlayOneShot(maxStep3, 0.42f);
-                break;
-            case "maxDeath":
-                audioSrc.PlayOneShot(maxDeath, 0.9f);
-                break;
+                case "maxStep1":
+                    gameAudio.PlayOneShot(maxStep1, 0.4f);
+                    break;
+                case "maxStep2":
+                    gameAudio.PlayOneShot(maxStep2, 0.35f);
+                    break;
+                case "maxStep3":
+                    gameAudio.PlayOneShot(maxStep3, 0.42f);
+                    break;
+                case "maxDeath":
+                    gameAudio.PlayOneShot(maxDeath, 0.9f);
+                    break;
 
-            case "alienStep1":
-                audioSrc.PlayOneShot(alienStep1);
-                break;
-            case "alienStep2":
-                audioSrc.PlayOneShot(alienStep2);
-                break;
-            case "alienStep3":
-                audioSrc.PlayOneShot(alienStep3);
-                break;
-            case "alienDeath":
-                audioSrc.PlayOneShot(alienDeath, 0.7f);
-                break;
+                case "alienStep1":
+                    gameAudio.PlayOneShot(alienStep1);
+                    break;
+                case "alienStep2":
+                    gameAudio.PlayOneShot(alienStep2);
+                    break;
+                case "alienStep3":
+                    gameAudio.PlayOneShot(alienStep3);
+                    break;
+                case "alienDeath":
+                    gameAudio.PlayOneShot(alienDeath, 0.7f);
+                    break;
 
-            case "mainMenu":
-                audioSrc.PlayOneShot(mainMenuTheme);
-                break;
+                case "mainMenu":
+                    gameAudio.PlayOneShot(mainMenuTheme);
+                    break;
 
-            case "theme01":
-                audioSrc.PlayOneShot(levelOneTheme, 0.9f);
-                audioSrc.loop = true;
-                break;
-             }
-        }
+                case "theme01":
+                    gameAudio.PlayOneShot(levelOneTheme, 0.9f);
+                    break;
+            }
+        }   
+    }
 
     //Methods for configuring the musicPaused bool
     public static void StopMusic()
     {
         musicPaused = true;
+        gameAudio.Pause();
     }
 
     public static void ContinueMusic()
     {
         musicPaused = false;
+        gameAudio.UnPause();
     }
 }
